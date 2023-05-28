d03abcc38307682e081604f78a62a3385bd7fa18
dieuht <dieuht3112@gmail.com>
2023-05-28 22:13:13 +0700

    update BT_JavaOOP2

--- JavaOOP/src/Person/Infomation.java
+++ JavaOOP/src/Person/Infomation.java
@@ -1,9 +1,22 @@
 package Person;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Infomation {
     public static void main(String[] args) {
-        System.out.println("Hiển thị tất cả thông tin của Person: ");
+        ArrayList<person> arrayPerson = new ArrayList<>();
         person p1 = new person("Sam", 25, "Nữ", "Hà Nội", "0987654321");
-        p1.getInfo();
+        person p2 = new person("Tôm", 29, "Nam", "Tiền Huân", "0911111111");
+        person p3 = new person("Tép", 27, "Nam", "Ngô Quyền", "09222222222");
+
+        arrayPerson.add(p1);
+        arrayPerson.add(p2);
+        arrayPerson.add(p3);
+
+        System.out.println("Hiển thị tất cả thông tin của Person: ");
+        for (person p:arrayPerson){
+            p.getInfo();
+        }
     }
 }
--- JavaOOP/src/company/Company.java
+++ JavaOOP/src/company/Company.java
@@ -2,10 +2,22 @@ package company;
 
 import Person.person;
 
+import java.util.ArrayList;
+
 public class Company {
     public static void main(String[] args) {
-        person p2 = new person("Sam", 25, "Nữ");
+        ArrayList<person> arrayPerson = new ArrayList<>();
+        person p1 = new person("Sam", 25, "Nữ");
+        person p2 = new person("Tôm", 29, "Nam");
+        person p3 = new person("Tép", 27, "Nam");
+
+        arrayPerson.add(p1);
+        arrayPerson.add(p2);
+        arrayPerson.add(p3);
+
         System.out.println("Các thông tin được public: ");
-        p2.getInfo();
+        for (person p : arrayPerson) {
+            p.getInfo();
+        }
     }
 }
